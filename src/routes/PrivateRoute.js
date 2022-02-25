d7cbc9150326bf7f796f202ff8c73b2ea6935b6e
unknown <nimalan1108@gmail.com>
2022-02-25 19:34:10 +0530

    fixed routingissues

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import Loader from "../components/Loader";
 import { useAuth } from "../contexts/authContext";
 
 const PrivateRoute = () => {
